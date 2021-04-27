2dca77d635d822ac7308cd2edb900bbb6e3a9365
Aaron M Watson <aaron.watson@unnpp.gov>
2021-04-27 06:14:44 -0400

    fixme: collect

--- crypto_collect.py
+++ crypto_collect.py
@@ -9,11 +9,20 @@ df = pd.DataFrame(columns=["time", "mark", "ask", "bid", "vol"])
 while True:
 
     try:
-        quote = get_next_price()
+        q = r.get_crypto_quote("BTC")
+        quote = {
+            "time": etime(datetime.now()),
+            "mark": float(q["mark_price"]),
+            "ask": float(q["ask_price"]),
+            "bid": float(q["bid_price"]),
+            "vol": float(q["volume"]),
+        }
+        print(quote)
         df = df.append(quote, ignore_index=True)
         sleep(5)
     except KeyboardInterrupt:
         df.to_csv(f"btc_data_5sec_{datetime.now():%Y%m%d-%H%M%S}.csv")
         break
-    except:
+    except Exception as e:
+        print(e)
         continue
